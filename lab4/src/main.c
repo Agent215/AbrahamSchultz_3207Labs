1257d85f4da6a4a522d30ea9c2ec57eb40e06201
Abraham Bricklin Schultz <abraham.schultz@gmail.com>
2019-11-18 10:30:05 -0500

    testing functions

--- lab4/src/main.c
+++ lab4/src/main.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "disk.h"
+
 int main (int argc , char** argv){
 
+	char * DiskName = "testDisk";
 
+	//int testDisk = make_disk(DiskName);
+	int error = open_disk(DiskName);
 
 return 0;
 }// end main
