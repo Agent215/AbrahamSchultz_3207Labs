29606ed81fce9f9611ceb9a9868f53d6bdbb8607
Abraham Bricklin Schultz <abraham.schultz@gmail.com>
2019-11-18 10:17:37 -0500

    working on structs

--- lab4/src/disk.h
+++ lab4/src/disk.h
@@ -16,4 +16,35 @@ int block_read(int block, char *buf);
                                /* read a block of size BLOCK_SIZE from disk   */
 /******************************************************************************/
 
+//Struct FAT{
+//	Int TotalBlocks = 16384
+//	Int UnusedBlocks;  // start at total blocks and subtract from as we go 
+//Vector <File > FAT // a linked list containing linked lists of blocks. }? // struct FAT The file struct will be a linked list of blocks 
+//Struct File{
+//	Vector <struct Block> blockList; // this will contain the block holding the data 
+//Int FileSize = blocklist.length * blockSize;
+//Int filePointer;   // pointer to current data byte 
+//Int startingAddr; // address of first block used by this file 
+//bool operator<(const startingAddr& rhs) const
+//{ // i think we can use this to compare file locations prioritizing lower address 
+//	return startingAddr> rhs.startingAddr;
+//} } ?// end Struct File The Block struct will be an element in the  
+//3
+//Struct Block{
+//	Char DATA[4096]  // these file will only hold text for now. 4096 bytes = 4KB 
+//	Int addrNextBlock; // point to next block used -1 if not used } ?// end Struct Block // used to initialize super block 
+//Struct BootSector{
+//	Int totalBlocks   // 16383 
+//	Int bytesPerBlock // 4096 
+//	String DiskName
+//	StartingBlocks    // blocks saved for initialization } ?// end struct BootSector // struct to keep track of directory hierarchies 
+//	Struct DirectoryList() {
+//	Int directCount;  // keep track of directories 
+//	Struct File* directList  // actual list referencing files that are directories  
+//}// end directoryList 
+//
+//
+
+
+
 #endif
